ee838b90aadac518780964da73826209578fc6f9
egumma <gumma.kondalu@gmail.com>
2020-06-16 14:04:57 +0530

    checkpoll1

--- src/main/java/com/prac/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/prac/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class NetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
 	}
 
